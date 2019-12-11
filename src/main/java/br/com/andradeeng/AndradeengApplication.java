079cb9bd23c980e2c5486fe89c3574828520b433
Vanius Silva Oliveira <vaniusmax@gmail.com>
2019-12-11 21:16:20 -0200

    Primeiro Commit

---
+++ src/main/java/br/com/andradeeng/AndradeengApplication.java
@@ -0,0 +1,13 @@
+package br.com.andradeeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndradeengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndradeengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andradeeng/AndradeengApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andradeeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndradeengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
