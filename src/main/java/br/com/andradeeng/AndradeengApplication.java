efca6b8e9c2809f81127070491f77faccca9dd4c
Vanius Silva Oliveira <vaniusmax@gmail.com>
2019-12-14 15:25:57 -0200

    Criando repositorio de produtos

--- src/main/java/br/com/andradeeng/AndradeengApplication.java
+++ src/main/java/br/com/andradeeng/AndradeengApplication.java
@@ -8,14 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.andradeeng.domain.Categoria;
+import br.com.andradeeng.domain.Produto;
 import br.com.andradeeng.repositories.CategoriaRepository;
-import br.com.andradeeng.services.CategoriaService;
+import br.com.andradeeng.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class AndradeengApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository repo;
+	
+	@Autowired
+	private ProdutoRepository repoProd;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(AndradeengApplication.class, args);
@@ -27,7 +32,21 @@ public class AndradeengApplication implements CommandLineRunner {
       Categoria cat1 = new Categoria(null, "Informatica");
       Categoria cat2 = new Categoria(null, "Escritório");
       
+      Produto p1 = new Produto(null, "Computador", 2000.00);
+      Produto p2 = new Produto(null, "Impressora", 800.00);
+      Produto p3 = new Produto(null, "Mouse", 80.00);
+      
+      cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+      cat2.getProdutos().addAll(Arrays.asList(p2));
+      
+      p1.getCategorias().addAll(Arrays.asList(cat1));
+      p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+      p3.getCategorias().addAll(Arrays.asList(cat1));
+      
+      
       repo.saveAll(Arrays.asList(cat1,cat2));
+      repoProd.saveAll(Arrays.asList(p1,p2,p3));
+      
 		
 	}
 
---
+++ src/main/java/br/com/andradeeng/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package br.com.andradeeng.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.andradeeng.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
