85d5bf190cd70dcd5bae0b5acaf0040b124e2627
Vanius Silva Oliveira <vaniusmax@gmail.com>
2019-12-14 11:37:00 -0200

    Criando classe de dominio Categoria

--- src/main/java/br/com/andradeeng/resources/CategoriaResource.java
+++ src/main/java/br/com/andradeeng/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package br.com.andradeeng.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.andradeeng.domain.Categoria;
 
 @RestController
 @RequestMapping(value="categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Computador");
+		Categoria cat2 = new Categoria(2, "Mouse");
+		
+		List<Categoria> list = new ArrayList<>();
+		
+		list.add(cat1);
+		list.add(cat2);
+		
+		
+		return list;
 	}
 
 }
