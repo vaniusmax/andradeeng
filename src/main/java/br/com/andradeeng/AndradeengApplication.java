88df7186695b8af89014db6a75b4e28f7c5dd379
Vanius Silva Oliveira <vaniusmax@gmail.com>
2019-12-14 14:38:32 -0200

    inserindo dados usando metodo save all

--- src/main/java/br/com/andradeeng/AndradeengApplication.java
+++ src/main/java/br/com/andradeeng/AndradeengApplication.java
@@ -1,13 +1,34 @@
 package br.com.andradeeng;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.andradeeng.domain.Categoria;
+import br.com.andradeeng.repositories.CategoriaRepository;
+import br.com.andradeeng.services.CategoriaService;
+
 @SpringBootApplication
-public class AndradeengApplication {
+public class AndradeengApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AndradeengApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+      Categoria cat1 = new Categoria(null, "Informatica");
+      Categoria cat2 = new Categoria(null, "Escritório");
+      
+      repo.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
